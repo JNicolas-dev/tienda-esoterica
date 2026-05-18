baa073fef0b1250d2a99e02e8a00fd4d0d94e3be
JNicolas-dev <nikolashernandez137@gmail.com>
2026-05-17 22:30:42 -0500

    aws backend connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-
 const cors = require('cors');
-
 const productosRoutes = require('./routes/productos.routes');
-
 const authRoutes = require('./routes/auth.routes');
-
 const app = express();
 
 app.use(cors());
